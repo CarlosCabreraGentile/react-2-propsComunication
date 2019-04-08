e9520afdf851adff2c419289f37981812da85dba
Carlos Cabrera <ccabrera@beerealit.com>
2019-04-08 10:06:21 -0300

    Comunication between components with props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    // One component inside of another
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Brad"
+          timeAgo="xxx"
+          textComment="zzz"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <ApprovalCard>
+        <CommentDetail
+          author="July"
+          timeAgo="xxx"
+          textComment="zzz"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Francis"
+          timeAgo="xxx"
+          textComment="zzz"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
